bd394f5307d5640c935618136c442e11f8a4ac82
Samuel <samuelvgnl@gmail.com>
2020-05-19 00:45:09 +0200

    Create contants.dart
    add color constants

---
+++ lib/contants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const TextColorL = Color(0xFF1E2432);
+const TextMediumColorL = Color(0xFF53627C);
+const TextLightColorL = Color(0xFFACB1C0);
+const PrimaryColorL = Color(0xFF0D8E53);
+const BackgroundColorL = Color(0xFFFCFCFC);
+const InactiveChartColorL = Color(0xFFEAECEF);
+
+const TextColorD = Color(0xFF1E2432);
+const TextMediumColorD = Color(0xFF53627C);
+const TextLightColorD = Color(0xFFACB1C0);
+const PrimaryColorD = Color(0xFF0D8E53);
+const BackgroundColorD = Color(0xFFFCFCFC);
+const InactiveChartColorD = Color(0xFFEAECEF);
