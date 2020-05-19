7cb3be89e90f54ee06e5f3f6c93fcdd922b1c282
Samuel <samuelvgnl@gmail.com>
2020-05-19 20:25:35 +0200

    Update api.dart
    form data in int

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -7,33 +7,46 @@ class Api {
   int totalDeaths;
   int totalRecovered;
   int newCase;
+  DateTime lastUpdate;
+  List<dynamic> lastWeek;
 
   Future<void> getData() async {
     try {
       Response response =
           await get('https://api.covid19api.com/country/france');
-      print(response.body);
-//      Map data = jsonDecode(response.body);
-//      print(data);
+      List<dynamic> data = jsonDecode(response.body);
+      // print(data);
+      // france metropo -> "Province": ""
+      List<dynamic> frenchMetropo = [];
+      data
+          .map((e) => e["Province"] == "" ? frenchMetropo.add(e) : null)
+          .toList();
+      print(frenchMetropo.length);
+      print(frenchMetropo);
+      // 7 dernier resultat
+      lastWeek = frenchMetropo.skip(frenchMetropo.length - 7).toList();
+      print(lastWeek);
+      print(lastWeek.length);
+      // filed
 
-      // get properties from data
-//      String datetime = data['datetime'];
-//      String offset = data['utc_offset'].substring(1, 3);
-      // print(datetime);
-      // print(offset);
+      confirmedCase = lastWeek[6]["Confirmed"];
+      totalDeaths = lastWeek[6]["Deaths"];
+      totalRecovered = lastWeek[6]["Recovered"];
+      newCase = lastWeek[6]["Confirmed"] - lastWeek[5]["Confirmed"];
+      lastUpdate = DateTime.parse(lastWeek[6]["Date"]);
 
-      // create DateTime Object
-//      DateTime now = DateTime.parse(datetime);
-//      now = now.add(Duration(hours: int.parse(offset)));
-//
-//      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
-//      time = DateFormat.jm().format(now);
+      print(confirmedCase);
+      print(totalDeaths);
+      print(totalRecovered);
+      print(newCase);
+      print(lastUpdate);
     } catch (e) {
       print('catch error: $e');
       confirmedCase = null;
       totalDeaths = null;
       totalRecovered = null;
       newCase = null;
+      lastUpdate = null;
     }
   }
 }
